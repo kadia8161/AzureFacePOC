e9796d4d78845e21d444762ebc769b1f8ad9b70d
Akash Kadia <Akash.Kadia@ftxinfotech.com>
2021-11-12 13:58:52 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace AzureFacePOC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.btnImageSelect = new System.Windows.Forms.Button();
+            this.txtImage = new System.Windows.Forms.TextBox();
+            this.txtWebCamImage = new System.Windows.Forms.TextBox();
+            this.btnBrowseCamImage = new System.Windows.Forms.Button();
+            this.btnAddFace = new System.Windows.Forms.Button();
+            this.txtCustomerId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.lblFaceAddProcess = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPersonId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCompareResult = new System.Windows.Forms.TextBox();
+            this.lblResultProcess = new System.Windows.Forms.Label();
+            this.personList = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFaceCustomData = new System.Windows.Forms.TextBox();
+            this.btnTrainPersonGroup = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.personList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(258, 481);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(194, 34);
+            this.btnCompare.TabIndex = 0;
+            this.btnCompare.Text = "Get Customer";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // btnImageSelect
+            // 
+            this.btnImageSelect.Location = new System.Drawing.Point(1144, 26);
+            this.btnImageSelect.Name = "btnImageSelect";
+            this.btnImageSelect.Size = new System.Drawing.Size(161, 34);
+            this.btnImageSelect.TabIndex = 1;
+            this.btnImageSelect.Text = "Browse";
+            this.btnImageSelect.UseVisualStyleBackColor = true;
+            this.btnImageSelect.Click += new System.EventHandler(this.btnImageSelect_Click);
+            // 
+            // txtImage
+            // 
+            this.txtImage.Enabled = false;
+            this.txtImage.Location = new System.Drawing.Point(258, 26);
+            this.txtImage.Name = "txtImage";
+            this.txtImage.Size = new System.Drawing.Size(880, 31);
+            this.txtImage.TabIndex = 2;
+            this.txtImage.Text = "Selfie Image...";
+            // 
+            // txtWebCamImage
+            // 
+            this.txtWebCamImage.Enabled = false;
+            this.txtWebCamImage.Location = new System.Drawing.Point(258, 420);
+            this.txtWebCamImage.Name = "txtWebCamImage";
+            this.txtWebCamImage.Size = new System.Drawing.Size(854, 31);
+            this.txtWebCamImage.TabIndex = 4;
+            this.txtWebCamImage.Text = "Webcam Captured Image";
+            // 
+            // btnBrowseCamImage
+            // 
+            this.btnBrowseCamImage.Location = new System.Drawing.Point(1118, 419);
+            this.btnBrowseCamImage.Name = "btnBrowseCamImage";
+            this.btnBrowseCamImage.Size = new System.Drawing.Size(136, 34);
+            this.btnBrowseCamImage.TabIndex = 3;
+            this.btnBrowseCamImage.Text = "Browse";
+            this.btnBrowseCamImage.UseVisualStyleBackColor = true;
+            this.btnBrowseCamImage.Click += new System.EventHandler(this.btnBrowseCamImage_Click);
+            // 
+            // btnAddFace
+            // 
+            this.btnAddFace.Location = new System.Drawing.Point(258, 170);
+            this.btnAddFace.Name = "btnAddFace";
+            this.btnAddFace.Size = new System.Drawing.Size(162, 34);
+            this.btnAddFace.TabIndex = 6;
+            this.btnAddFace.Text = "Register Face";
+            this.btnAddFace.UseVisualStyleBackColor = true;
+            this.btnAddFace.Click += new System.EventHandler(this.btnAddFace_Click);
+            // 
+            // txtCustomerId
+            // 
+            this.txtCustomerId.Location = new System.Drawing.Point(763, 78);
+            this.txtCustomerId.Name = "txtCustomerId";
+            this.txtCustomerId.Size = new System.Drawing.Size(220, 31);
+            this.txtCustomerId.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(647, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Customer Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 419);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Select Webcam Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Select Selfie Image";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Customer Name";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(258, 78);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(383, 31);
+            this.txtCustomerName.TabIndex = 12;
+            // 
+            // lblFaceAddProcess
+            // 
+            this.lblFaceAddProcess.AutoSize = true;
+            this.lblFaceAddProcess.Location = new System.Drawing.Point(440, 175);
+            this.lblFaceAddProcess.Name = "lblFaceAddProcess";
+            this.lblFaceAddProcess.Size = new System.Drawing.Size(110, 25);
+            this.lblFaceAddProcess.TabIndex = 13;
+            this.lblFaceAddProcess.Text = "Adding Face";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Person Id";
+            // 
+            // txtPersonId
+            // 
+            this.txtPersonId.Location = new System.Drawing.Point(258, 126);
+            this.txtPersonId.Name = "txtPersonId";
+            this.txtPersonId.Size = new System.Drawing.Size(383, 31);
+            this.txtPersonId.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(647, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(302, 25);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "(Add Multiple Face For Same Person)";
+            // 
+            // txtCompareResult
+            // 
+            this.txtCompareResult.Location = new System.Drawing.Point(258, 531);
+            this.txtCompareResult.Multiline = true;
+            this.txtCompareResult.Name = "txtCompareResult";
+            this.txtCompareResult.ReadOnly = true;
+            this.txtCompareResult.Size = new System.Drawing.Size(854, 142);
+            this.txtCompareResult.TabIndex = 17;
+            // 
+            // lblResultProcess
+            // 
+            this.lblResultProcess.AutoSize = true;
+            this.lblResultProcess.Location = new System.Drawing.Point(458, 486);
+            this.lblResultProcess.Name = "lblResultProcess";
+            this.lblResultProcess.Size = new System.Drawing.Size(109, 25);
+            this.lblResultProcess.TabIndex = 18;
+            this.lblResultProcess.Text = "Processing...";
+            // 
+            // personList
+            // 
+            this.personList.AllowUserToAddRows = false;
+            this.personList.AllowUserToDeleteRows = false;
+            this.personList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.personList.Location = new System.Drawing.Point(258, 211);
+            this.personList.Name = "personList";
+            this.personList.ReadOnly = true;
+            this.personList.RowHeadersWidth = 62;
+            this.personList.RowTemplate.Height = 33;
+            this.personList.Size = new System.Drawing.Size(1129, 172);
+            this.personList.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1011, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 25);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Face Data";
+            // 
+            // txtFaceCustomData
+            // 
+            this.txtFaceCustomData.Location = new System.Drawing.Point(1105, 78);
+            this.txtFaceCustomData.Name = "txtFaceCustomData";
+            this.txtFaceCustomData.Size = new System.Drawing.Size(220, 31);
+            this.txtFaceCustomData.TabIndex = 20;
+            // 
+            // btnTrainPersonGroup
+            // 
+            this.btnTrainPersonGroup.Location = new System.Drawing.Point(1178, 170);
+            this.btnTrainPersonGroup.Name = "btnTrainPersonGroup";
+            this.btnTrainPersonGroup.Size = new System.Drawing.Size(198, 34);
+            this.btnTrainPersonGroup.TabIndex = 22;
+            this.btnTrainPersonGroup.Text = "Train Person Group";
+            this.btnTrainPersonGroup.UseVisualStyleBackColor = true;
+            this.btnTrainPersonGroup.Click += new System.EventHandler(this.btnTrainPersonGroup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1707, 698);
+            this.Controls.Add(this.btnTrainPersonGroup);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtFaceCustomData);
+            this.Controls.Add(this.personList);
+            this.Controls.Add(this.lblResultProcess);
+            this.Controls.Add(this.txtCompareResult);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPersonId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblFaceAddProcess);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCustomerId);
+            this.Controls.Add(this.btnAddFace);
+            this.Controls.Add(this.txtWebCamImage);
+            this.Controls.Add(this.btnBrowseCamImage);
+            this.Controls.Add(this.txtImage);
+            this.Controls.Add(this.btnImageSelect);
+            this.Controls.Add(this.btnCompare);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+            ((System.ComponentModel.ISupportInitialize)(this.personList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Button btnImageSelect;
+        private System.Windows.Forms.TextBox txtImage;
+        private System.Windows.Forms.TextBox txtWebCamImage;
+        private System.Windows.Forms.Button btnBrowseCamImage;
+        private System.Windows.Forms.Button btnAddFace;
+        private System.Windows.Forms.TextBox txtCustomerId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label lblFaceAddProcess;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPersonId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCompareResult;
+        private System.Windows.Forms.Label lblResultProcess;
+        private System.Windows.Forms.DataGridView personList;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFaceCustomData;
+        private System.Windows.Forms.Button btnTrainPersonGroup;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System.IO;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AzureFacePOC
+{
+    public partial class Form1 : Form
+    {
+        const string FACE_SUBSCRIPTION_KEY = "Azure_Face_Api_Key";
+        const string FACE_ENDPOINT = "Azure_Face_Api_Endpoint";
+        const string RECOGNITION_MODEL4 = RecognitionModel.Recognition04;
+        const string DETECTION_MODEL3 = DetectionModel.Detection03;
+        private OpenFileDialog openFileDialog1;
+        private OpenFileDialog openFileDialog2;
+        private readonly IFaceClient _faceClient;
+        const string LP_GROUP_ID = "largepersongroup01";
+        const string LP_GROUP_NAME = "LargePersonGroup";
+        public Form1()
+        {
+            InitializeComponent();
+            _faceClient = new FaceClient(new ApiKeyServiceClientCredentials(FACE_SUBSCRIPTION_KEY)) { Endpoint = FACE_ENDPOINT };
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.FileOk += OpenFileDialog1_FileOk;
+            openFileDialog2 = new OpenFileDialog();
+            openFileDialog2.FileOk += OpenFileDialog2_FileOk;
+            //FaceDetection.Initialize(FACE_ENDPOINT, FACE_SUBSCRIPTION_KEY, DETECTION_MODEL3);
+            BindPersonList();
+        }
+        private void BindPersonList()
+        {
+            IList<Person> persons = GetPersonList();
+            DataTable dtPerson = new DataTable();
+            dtPerson.Columns.Add("Person Id", typeof(string));
+            dtPerson.Columns.Add("Name", typeof(string));
+            dtPerson.Columns.Add("Customer Id", typeof(string));
+            dtPerson.Columns.Add("Face ID", typeof(string));
+            dtPerson.Columns.Add("Face Details", typeof(string));
+            DataRow dr;
+            foreach (var per in persons)
+            {
+                dr = dtPerson.NewRow();
+                dr["Person Id"] = per.PersonId.ToString();
+                dr["Name"] = per.Name;
+                dr["Customer Id"] = per.UserData;
+                dr["Face ID"] = per.PersistedFaceIds[0]; //string.Join(",", per.PersistedFaceIds);
+                dr["Face Details"] = GetPersonFaceDetails(LP_GROUP_ID, per.PersonId, per.PersistedFaceIds[0]);
+                dtPerson.Rows.Add(dr);
+                if (per.PersistedFaceIds.Count > 1)
+                {
+                    for (int i = 1; i < per.PersistedFaceIds.Count; i++)
+                    {
+                        dr = dtPerson.NewRow();
+                        dr["Face ID"] = per.PersistedFaceIds[i];
+                        dr["Face Details"] = GetPersonFaceDetails(LP_GROUP_ID, per.PersonId, per.PersistedFaceIds[i]);
+                        dtPerson.Rows.Add(dr);
+                    }
+                }
+            }
+            personList.DataSource = dtPerson;
+        }
+
+        private string GetPersonFaceDetails(string largePersonGroupId, Guid personId, Guid faceId)
+        {
+            PersistedFace persistedFace = _faceClient.LargePersonGroupPerson.GetFaceAsync(largePersonGroupId, personId, faceId).GetAwaiter().GetResult();
+            return persistedFace.UserData;
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            lblResultProcess.Text = "Loading...";
+            //Image image_Photo = Image.FromFile(txtImage.Text);
+            //Image image_ID = Image.FromFile(txtID.Text);
+            string imageFaceId;
+            using (FileStream fileStream = File.OpenRead(txtWebCamImage.Text))
+            {
+                imageFaceId = GetFaceId(_faceClient, fileStream);
+            }
+            //string imageFaceId = GetFaceId(_faceClient, image_Photo);
+            //string icardFaceId = GetFaceId(_faceClient, image_ID);
+            lblResultProcess.Text = "Completed";
+            txtCompareResult.Text = ComparaFace(_faceClient, imageFaceId);
+        }
+
+        private string GetFaceId(IFaceClient faceClient, Stream imageStream)
+        {
+            try
+            {
+                string faceId = null;
+                if (faceClient != null)
+                {
+                    IList<DetectedFace> detectedFaces = faceClient.Face.DetectWithStreamAsync(image: imageStream, returnFaceId: true, faceIdTimeToLive: 300, detectionModel: DETECTION_MODEL3,
+                        recognitionModel: RECOGNITION_MODEL4).Result;
+                    if (detectedFaces.Count > 0)
+                    {
+                        faceId = detectedFaces[0].FaceId.Value.ToString();
+                    }
+                }
+                return faceId;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private string ComparaFace(IFaceClient faceClient, string imageGuid)
+        {
+            try
+            {
+                //List<Guid?> compareGuids = new List<Guid?>();
+                //compareGuids.Add(Guid.Parse(idGuid));
+
+                IList<IdentifyResult> similarFaces = faceClient.Face.IdentifyAsync(new List<Guid>() { Guid.Parse(imageGuid) },
+                    null, LP_GROUP_ID, 5, 0.5d).GetAwaiter().GetResult();
+                //Face.FindSimilarAsync(Guid.Parse(imageGuid), 
+                // null, null, compareGuids, 10, FindSimilarMatchMode.MatchFace).Result;
+                //VerifyResult result = faceClient.Face.VerifyFaceToFaceAsync(Guid.Parse(imageGuid), Guid.Parse(idGuid)).GetAwaiter().GetResult();
+                //return "Confidence " + result.Confidence.ToString("#0.#####") + " IsIdentical : " + (result.IsIdentical ? "Yes" : "No");
+                if (similarFaces.Any())
+                {
+                    StringBuilder sbResult = new StringBuilder();
+                    sbResult.AppendLine("Find Following Customers");
+                    Person matchedPerson;
+                    foreach (var candidate in similarFaces[0].Candidates)
+                    {
+                        matchedPerson = GetPerson(LP_GROUP_ID, candidate.PersonId);
+                        sbResult.AppendLine($"Name : {matchedPerson.Name} CustomerId : {matchedPerson.UserData} Confidence : {candidate.Confidence.ToString("#0.#####")}");
+                    }
+                    return sbResult.ToString();
+                }
+                else
+                {
+                    return "No Match Found";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private Person GetPerson(string largePersonGroupId, Guid personId)
+        {
+            return _faceClient.LargePersonGroupPerson.GetAsync(largePersonGroupId, personId).GetAwaiter().GetResult();
+        }
+
+        private void btnImageSelect_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            txtImage.Text = ((FileDialog)sender).FileName;
+        }
+
+        private void btnBrowseCamImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void OpenFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            txtWebCamImage.Text = ((FileDialog)sender).FileName;
+        }
+
+        private void btnAddFace_Click(object sender, EventArgs e)
+        {
+
+            lblFaceAddProcess.Text = "Processing...";
+            using (Stream image = File.OpenRead(txtImage.Text))
+            {
+                AddFaceToPersonGroup(image, txtCustomerId.Text.Trim(), txtPersonId.Text.Trim(), txtFaceCustomData.Text.Trim());
+            }
+            BindPersonList();
+        }
+
+        private void AddFaceToPersonGroup(Stream image, string customerId, string personId, string faceCustomData)
+        {
+            try
+            {
+                bool groupExists = CheckLargePersonGroupExists(LP_GROUP_ID);
+                if (!groupExists)
+                {
+                    lblFaceAddProcess.Text = "Creating Person Group...";
+                    CreateLargePersonGroup(LP_GROUP_ID, LP_GROUP_NAME, "Store Person Data");
+                }
+                Guid personGuid = new Guid();
+                if (string.IsNullOrWhiteSpace(personId))
+                {
+                    lblFaceAddProcess.Text = "Creating Person...";
+                    personGuid = CreateLargePersonGroupPerson(LP_GROUP_ID, txtCustomerName.Text, customerId);
+                }
+                else
+                {
+                    personGuid = Guid.Parse(personId);
+                }
+                lblFaceAddProcess.Text = "Adding Face For Person...";
+                Guid personFaceId = AddFaceToLargePersonGroupFromStream(LP_GROUP_ID, personGuid, image, faceCustomData);
+                MessageBox.Show($"Face with FaceId : {personFaceId} Added For PersonId : {personGuid} Under Person Group : {LP_GROUP_ID}");
+                lblFaceAddProcess.Text = "Completed";
+                ClearControl();
+                BindPersonList();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            _faceClient.Dispose();
+        }
+        private void ClearControl()
+        {
+            txtImage.Text = "";
+            txtWebCamImage.Text = "";
+            txtCustomerId.Text = "";
+            txtPersonId.Text = "";
+            txtCustomerName.Text = "";
+            txtFaceCustomData.Text = "";
+            //lblMatch.Text = "Confidence 0.0";
+        }
+
+        public void CreateLargePersonGroup(string largePersonGroupId, string largePersonGroupName, string customData)
+        {
+            _faceClient.LargePersonGroup.CreateAsync(largePersonGroupId, largePersonGroupName, customData, RECOGNITION_MODEL4).GetAwaiter().GetResult();
+        }
+        public Guid CreateLargePersonGroupPerson(string largePersonGroupId, string personName, string customData)
+        {
+            Person person = _faceClient.LargePersonGroupPerson.CreateAsync(largePersonGroupId, personName, customData).GetAwaiter().GetResult();
+            return person.PersonId;
+        }
+        public Guid AddFaceToLargePersonGroupFromStream(string largePersonGroupId, Guid personId, Stream personImage,
+            string customData = null, IList<int> faceRectangle = null)
+        {
+            PersistedFace persistedFace = _faceClient.LargePersonGroupPerson
+                .AddFaceFromStreamAsync(largePersonGroupId, personId, personImage, customData,
+                faceRectangle, DETECTION_MODEL3).GetAwaiter().GetResult();
+            return persistedFace.PersistedFaceId;
+        }
+        public bool CheckLargePersonGroupExists(string largePersonGroupId)
+        {
+            try
+            {
+                LargePersonGroup largePersonGroup = _faceClient.LargePersonGroup.GetAsync(largePersonGroupId).GetAwaiter().GetResult();
+                return largePersonGroup != null;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public IList<Person> GetPersonList()
+        {
+            return _faceClient.LargePersonGroupPerson.ListAsync(LP_GROUP_ID, null, 10).GetAwaiter().GetResult();
+        }
+
+        private void btnTrainPersonGroup_Click(object sender, EventArgs e)
+        {
+            lblFaceAddProcess.Text = "Train Person Group Started";
+            TrainPersonGroup(LP_GROUP_ID);
+            lblFaceAddProcess.Text = "Train Person Group Completed";
+        }
+
+        private void TrainPersonGroup(string largePersonGroupId)
+        {
+            Task trainTask = _faceClient.LargePersonGroup.TrainAsync(largePersonGroupId);
+            do
+            {
+                Task.Delay(1000);
+            }
+            while (!trainTask.IsCompleted);
+        }
+    }
+}
